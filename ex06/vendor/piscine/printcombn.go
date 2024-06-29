432e1d84d8b96d8a298629e4f3ca509cc831e26b
Kudo Tomoya <ktomoya@c3r5s6.42tokyo.jp>
2024-06-29 16:53:24 +0900

    Added ex06 directory

---
+++ ex06/vendor/piscine/printcombn.go
@@ -0,0 +1,39 @@
+package	piscine
+
+import "ft"
+
+func PrintCombN() {
+	left_tens := '0'
+	left_units := '0'
+	right_tens := '0'
+	right_units := left_units + 1
+
+	for left_tens <= '9' {
+		for left_units <= '9' {
+			for right_tens <= '9' {
+				for right_units <= '9' {
+					ft.PrintRune(left_tens)
+					ft.PrintRune(left_units)
+					ft.PrintRune(' ')
+					ft.PrintRune(right_tens)
+					ft.PrintRune(right_units)
+					if (left_tens != '9' || left_units != '8') {
+						ft.PrintRune(',')
+						ft.PrintRune(' ')
+					}
+					right_units++
+				}
+				right_tens++
+				right_units = '0'
+			}
+			left_units++
+			right_tens = left_tens
+			right_units = left_units + 1
+		}
+		left_tens++
+		left_units = '0'
+		right_tens = left_tens
+		right_units = left_units + 1
+	}
+	ft.PrintRune('\n')
+}
